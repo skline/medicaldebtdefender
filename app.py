5010152c0a6b34f3f30be589548b8911bf030331
Spencer Kline <spencerkline@Spencers-MacBook-Pro.local>
2023-12-17 11:35:04 -0700

    adds new api call

--- app.py
+++ app.py
@@ -1,9 +1,51 @@
-from flask import Flask, render_template, request, Response
+from flask import Flask, render_template, request, Response, jsonify
 import yaml
-from openai import OpenAI
+import openai
 from markupsafe import Markup
-client = OpenAI()
 app = Flask(__name__)
+import psycopg2
+import os
+
+# Database connection parameters
+DB_HOST = 'dbmdd.postgres.database.azure.com'
+DB_NAME = 'postgres'
+DB_USER = 'skline'
+DB_PASS = os.getenv('DB_PASS')
+
+@app.route('/get_real_name', methods=['GET'])
+def get_real_name():
+    # Get the 'name' from the query parameters
+    name = request.args.get('name')
+    print(DB_PASS)
+    # Connect to the database
+    conn = psycopg2.connect(
+        dbname=DB_NAME, 
+        user=DB_USER, 
+        password=DB_PASS, 
+        host=DB_HOST
+    )
+
+    # Create a cursor object
+    cur = conn.cursor()
+
+    # Query the database for the real name
+    cur.execute('SELECT real_name FROM client_names WHERE name = %s', (name,))
+    result = cur.fetchone()
+
+    # Close the cursor and the connection
+    cur.close()
+    conn.close()
+
+    # If no result is found, return an error
+    if result is None:
+        response = jsonify({'error': 'Name not found'}), 404
+    else:
+        # If a result is found, return the real name
+        real_name = result[0]
+        response = jsonify({'real_name': real_name}), 200
+
+    return response
+
 
 @app.route('/')
 def form():
@@ -19,7 +61,7 @@ def submit_form():
     yaml_data = yaml.dump(form_data, default_flow_style=False)
 
 
-    completion = client.chat.completions.create(
+    completion = openai.ChatCompletion.create(
     model="gpt-4-1106-preview",
     messages=[
         {"role": "system", "content": "You are a medical debt advocate who is helping a patient create a letter to their medical provider. The letter should primarily consist of specific and polite questions seeking clarification and assistance regarding their medical debt. Ensure the letter is respectful and professional."},
